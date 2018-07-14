b77675ab4cee9c6a2b6903dddc69c5a04ddcb66e
高帅朋 <gaoshuaipeng@ecloudinfo.com>
2018-07-14 12:19:01 +0800

    调整项目结构

---
+++ app/controllers/auth.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"fmt"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/wangsongyan/wblog/app/helpers"
+	"net/http"
+	"github.com/wangsongyan/wblog/config"
+)
+
+func AuthGet(c *gin.Context) {
+	authType := c.Param("authType")
+
+	session := sessions.Default(c)
+	uuid := helpers.UUID()
+	session.Delete(SESSION_GITHUB_STATE)
+	session.Set(SESSION_GITHUB_STATE, uuid)
+	session.Save()
+
+	authurl := "/signin"
+	switch authType {
+	case "github":
+		authurl = fmt.Sprintf(config.GetConfiguration().GithubAuthUrl, config.GetConfiguration().GithubClientId, uuid)
+	case "weibo":
+	case "qq":
+	case "wechat":
+	case "oschina":
+	default:
+	}
+	c.Redirect(http.StatusFound, authurl)
+}
---
+++ config/routers/routes.go
@@ -0,0 +1,206 @@
+package routers
+
+import (
+	"github.com/wangsongyan/wblog/app/controllers"
+	"github.com/gin-gonic/gin"
+	"path/filepath"
+	"github.com/wangsongyan/wblog/app/models"
+	"github.com/cihub/seelog"
+	"net/http"
+	"github.com/gin-contrib/sessions"
+	"github.com/wangsongyan/wblog/app/helpers"
+	"html/template"
+	"github.com/wangsongyan/wblog/config"
+)
+
+func SetRouter(router *gin.Engine) {
+	setTemplate(router)
+	setSessions(router)
+	router.Use(SharedData())
+	router.Static("./public", filepath.Join("", "./public"))
+	router.NoRoute(controllers.Handle404)
+	router.GET("/", controllers.IndexGet)
+	router.GET("/index", controllers.IndexGet)
+	router.GET("/rss", controllers.RssGet)
+
+	if config.GetConfiguration().SignupEnabled {
+		router.GET("/signup", controllers.SignupGet)
+		router.POST("/signup", controllers.SignupPost)
+	}
+	// user signin and logout
+	router.GET("/signin", controllers.SigninGet)
+	router.POST("/signin", controllers.SigninPost)
+	router.GET("/logout", controllers.LogoutGet)
+	router.GET("/oauth2callback", controllers.Oauth2Callback)
+	router.GET("/auth/:authType", controllers.AuthGet)
+
+	// captcha
+	router.GET("/captcha", controllers.CaptchaGet)
+
+	visitor := router.Group("/visitor")
+	visitor.Use(AuthRequired())
+	{
+		visitor.POST("/new_comment", controllers.CommentPost)
+		visitor.POST("/comment/:id/delete", controllers.CommentDelete)
+	}
+
+	// subscriber
+	router.GET("/subscribe", controllers.SubscribeGet)
+	router.POST("/subscribe", controllers.Subscribe)
+	router.GET("/active", controllers.ActiveSubsciber)
+	router.GET("/unsubscribe", controllers.UnSubscribe)
+
+	router.GET("/page/:id", controllers.PageGet)
+	router.GET("/post/:id", controllers.PostGet)
+	router.GET("/tag/:tag", controllers.TagGet)
+	router.GET("/archives/:year/:month", controllers.ArchiveGet)
+
+	router.GET("/link/:id", controllers.LinkGet)
+
+	authorized := router.Group("/admin")
+	authorized.Use(AdminScopeRequired())
+	{
+		// index
+		authorized.GET("/index", controllers.AdminIndex)
+
+		// image upload
+		authorized.POST("/upload", controllers.Upload)
+
+		// page
+		authorized.GET("/page", controllers.PageIndex)
+		authorized.GET("/new_page", controllers.PageNew)
+		authorized.POST("/new_page", controllers.PageCreate)
+		authorized.GET("/page/:id/edit", controllers.PageEdit)
+		authorized.POST("/page/:id/edit", controllers.PageUpdate)
+		authorized.POST("/page/:id/publish", controllers.PagePublish)
+		authorized.POST("/page/:id/delete", controllers.PageDelete)
+
+		// post
+		authorized.GET("/post", controllers.PostIndex)
+		authorized.GET("/new_post", controllers.PostNew)
+		authorized.POST("/new_post", controllers.PostCreate)
+		authorized.GET("/post/:id/edit", controllers.PostEdit)
+		authorized.POST("/post/:id/edit", controllers.PostUpdate)
+		authorized.POST("/post/:id/publish", controllers.PostPublish)
+		authorized.POST("/post/:id/delete", controllers.PostDelete)
+
+		// tag
+		authorized.POST("/new_tag", controllers.TagCreate)
+
+		//
+		authorized.GET("/user", controllers.UserIndex)
+		authorized.POST("/user/:id/lock", controllers.UserLock)
+
+		// profile
+		authorized.GET("/profile", controllers.ProfileGet)
+		authorized.POST("/profile", controllers.ProfileUpdate)
+		authorized.POST("/profile/email/bind", controllers.BindEmail)
+		authorized.POST("/profile/email/unbind", controllers.UnbindEmail)
+		authorized.POST("/profile/github/unbind", controllers.UnbindGithub)
+
+		// subscriber
+		authorized.GET("/subscriber", controllers.SubscriberIndex)
+		authorized.POST("/subscriber", controllers.SubscriberPost)
+
+		// link
+		authorized.GET("/link", controllers.LinkIndex)
+		authorized.POST("/new_link", controllers.LinkCreate)
+		authorized.POST("/link/:id/edit", controllers.LinkUpdate)
+		authorized.POST("/link/:id/delete", controllers.LinkDelete)
+
+		// comment
+		authorized.POST("/comment/:id", controllers.CommentRead)
+		authorized.POST("/read_all", controllers.CommentReadAll)
+
+		// backup
+		authorized.POST("/backup", controllers.BackupPost)
+		authorized.POST("/restore", controllers.RestorePost)
+
+		// mail
+		authorized.POST("/new_mail", controllers.SendMail)
+		authorized.POST("/new_batchmail", controllers.SendBatchMail)
+	}
+}
+
+func AuthRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if user, _ := c.Get(controllers.CONTEXT_USER_KEY); user != nil {
+			if _, ok := user.(*models.User); ok {
+				c.Next()
+				return
+			}
+		}
+		seelog.Warnf("User not authorized to visit %s", c.Request.RequestURI)
+		c.HTML(http.StatusForbidden, "errors/error.html", gin.H{
+			"message": "Forbidden!",
+		})
+		c.Abort()
+	}
+}
+
+//AuthRequired grants access to authenticated users, requires SharedData middleware
+func AdminScopeRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if user, _ := c.Get(controllers.CONTEXT_USER_KEY); user != nil {
+			if u, ok := user.(*models.User); ok && u.IsAdmin {
+				c.Next()
+				return
+			}
+		}
+		seelog.Warnf("User not authorized to visit %s", c.Request.RequestURI)
+		c.HTML(http.StatusForbidden, "errors/error.html", gin.H{
+			"message": "Forbidden!",
+		})
+		c.Abort()
+	}
+}
+
+//SharedData fills in common data, such as user info, etc...
+func SharedData() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		if uID := session.Get(controllers.SESSION_KEY); uID != nil {
+			user, err := models.GetUser(uID)
+			if err == nil {
+				c.Set(controllers.CONTEXT_USER_KEY, user)
+			}
+		}
+		if config.GetConfiguration().SignupEnabled {
+			c.Set("SignupEnabled", true)
+		}
+		c.Next()
+	}
+}
+
+func setTemplate(engine *gin.Engine) {
+
+	funcMap := template.FuncMap{
+		"dateFormat": helpers.DateFormat,
+		"substring":  helpers.Substring,
+		"isOdd":      helpers.IsOdd,
+		"isEven":     helpers.IsEven,
+		"truncate":   helpers.Truncate,
+		"add":        helpers.Add,
+		"listtag":    helpers.ListTag,
+	}
+
+	engine.SetFuncMap(funcMap)
+	engine.LoadHTMLGlob(filepath.Join("", "./app/views/**/*"))
+}
+
+//setSessions initializes sessions & csrf middlewares
+func setSessions(router *gin.Engine) {
+	config := config.GetConfiguration()
+	//https://github.com/gin-gonic/contrib/tree/master/sessions
+	store := sessions.NewCookieStore([]byte(config.SessionSecret))
+	store.Options(sessions.Options{HttpOnly: true, MaxAge: 7 * 86400, Path: "/"}) //Also set Secure: true if using SSL, you should though
+	router.Use(sessions.Sessions("gin-session", store))
+	//https://github.com/utrack/gin-csrf
+	/*router.Use(csrf.Middleware(csrf.Options{
+		Secret: config.SessionSecret,
+		ErrorFunc: func(c *gin.Context) {
+			c.String(400, "CSRF token mismatch")
+			c.Abort()
+		},
+	}))*/
+}
